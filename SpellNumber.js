c8c2eb7771cbbf3ee815e80c47f9d7072982c155
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-11-03 15:06:51 +0530

    REFACTOR: if number is not correct then show proper output

--- SpellNumber.js
+++ SpellNumber.js
@@ -3,8 +3,8 @@ const NUMBER_SPELLINGS = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", 
 const readline = require("readline-sync");
   
 let number = Number(readline.question("Enter single digit number : "));
-try{
+if(NUMBER_SPELLINGS[number]){
     console.log("You entered " + NUMBER_SPELLINGS[number]+".");
-} catch(e){
+} else {
     console.log("You did not enterd single digit number.");
 }
